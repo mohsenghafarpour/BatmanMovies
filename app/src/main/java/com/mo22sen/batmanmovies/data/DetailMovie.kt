30e269dbf397328bbf9f1842d3cd6ba07bb53d4f
mo22sen <mohsenghafarpoor93@gmail.com>
2020-03-05 22:21:48 +0330

    added db for detail movie

--- app/src/main/java/com/mo22sen/batmanmovies/data/DetailMovie.kt
+++ app/src/main/java/com/mo22sen/batmanmovies/data/DetailMovie.kt
@@ -1,64 +1,87 @@
 package com.mo22sen.batmanmovies.data
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
+private const val TABLE_NAME = "detail"
+
+@Entity(tableName = TABLE_NAME)
 data class DetailMovie(
     @SerializedName("Title")
+    @ColumnInfo(name = "Title")
     val title: String,
     @SerializedName("Year")
+    @ColumnInfo(name = "Year")
     val year: String,
     @SerializedName("Rated")
+    @ColumnInfo(name = "Rated")
     val rated: String,
     @SerializedName("Released")
+    @ColumnInfo(name = "Released")
     val released: String,
     @SerializedName("Runtime")
+    @ColumnInfo(name = "Runtime")
     val runTime: String,
     @SerializedName("Genre")
+    @ColumnInfo(name = "Genre")
     val genre: String,
     @SerializedName("Director")
+    @ColumnInfo(name = "Director")
     val director: String,
     @SerializedName("Writer")
+    @ColumnInfo(name = "Writer")
     val writer: String,
     @SerializedName("Actors")
+    @ColumnInfo(name = "Actors")
     val actors: String,
     @SerializedName("Plot")
+    @ColumnInfo(name = "Plot")
     val plot: String,
     @SerializedName("Language")
+    @ColumnInfo(name = "Language")
     val language: String,
     @SerializedName("Country")
+    @ColumnInfo(name = "Country")
     val country: String,
     @SerializedName("Awards")
+    @ColumnInfo(name = "Awards")
     val awards: String,
     @SerializedName("Poster")
+    @ColumnInfo(name = "Poster")
     val poster: String,
-    @SerializedName("Ratings")
-    val rating: List<Rating>,
     @SerializedName("Metascore")
+    @ColumnInfo(name = "Metascore")
     val metaScore: String,
     @SerializedName("imdbRating")
+    @ColumnInfo(name = "imdbRating")
     val imdbRating: String,
     @SerializedName("imdbVotes")
+    @ColumnInfo(name = "imdbVotes")
     val imdbVotes: String,
+    @PrimaryKey
     @SerializedName("imdbID")
+    @ColumnInfo(name = "imdbID")
     val imdbId: String,
     @SerializedName("Type")
+    @ColumnInfo(name = "Type")
     val type: String,
     @SerializedName("DVD")
+    @ColumnInfo(name = "DVD")
     val dvd: String,
     @SerializedName("BoxOffice")
+    @ColumnInfo(name = "BoxOffice")
     val boxOffice: String,
     @SerializedName("Production")
+    @ColumnInfo(name = "Production")
     val production: String,
     @SerializedName("Website")
+    @ColumnInfo(name = "Website")
     val website: String,
     @SerializedName("Response")
+    @ColumnInfo(name = "Response")
     val response: String
 )
 
 
-data class Rating(
-    @SerializedName("Source")
-    val source: String,
-    @SerializedName("Value")
-    val value: String
-)
--- app/src/main/java/com/mo22sen/batmanmovies/data/db/BatmanMoviesDB.kt
+++ app/src/main/java/com/mo22sen/batmanmovies/data/db/BatmanMoviesDB.kt
@@ -2,12 +2,13 @@ package com.mo22sen.batmanmovies.data.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import com.mo22sen.batmanmovies.data.Movie
+import com.mo22sen.batmanmovies.data.DetailMovie
 import com.mo22sen.batmanmovies.data.Search
 
 @Database(
     entities = [
-        Search::class
+        Search::class,
+        DetailMovie::class
     ], version = 1, exportSchema = false
 )
 
